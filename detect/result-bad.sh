2298b3bee9c711ebb76be71f63f9faa302710bed
David Scott <dave.scott@docker.com>
2019-06-14 14:26:41 +0100

    Add missing scripts
    Signed-off-by: David Scott <dave.scott@docker.com>

---
+++ detect/result-bad.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Spam all the consoles
+for console in tty0 ttyS0 ttyAMA0 ttysclp0 stdout
+do
+  echo Writing to /dev/${console}
+  echo "Kernel is broken :(" | figlet > /dev/${console}
+done
+
+sleep 5s
+halt
